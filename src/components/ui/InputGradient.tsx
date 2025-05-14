9f04a60b284aa2228b67f289f430127e3ce5e160
hessuew <juhanijuusola@gmail.com>
2025-05-14 20:25:53 +0300

    refactor: removing old images and adding email section

--- src/components/ui/InputGradient.tsx
+++ src/components/ui/InputGradient.tsx
@@ -4,7 +4,7 @@ import { Send } from 'lucide-react';
 
 export function InputGradient({ text }: { text: string }) {
   return (
-    <div className='group relative flex pb-1 items-center justify-center rounded-full px-8 pt-1 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] '>
+    <div className='group relative flex h-12 pb-1 items-center justify-center rounded-full px-8 pt-1 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] '>
       <span
         className={cn(
           'absolute inset-0 block h-full w-full animate-gradient rounded-[inherit] bg-gradient-to-r from-[#ffaa40]/50 via-[#9c40ff]/50 to-[#ffaa40]/50 bg-[length:300%_100%] p-0.5'
@@ -19,7 +19,7 @@ export function InputGradient({ text }: { text: string }) {
       />
       <AnimatedGradientText className='text-base font-semibold'>{text}</AnimatedGradientText>
       <Send
-        className='ml-1 size-4 stroke-neutral-500 transition-transform
+        className='ml-2 size-4 stroke-neutral-500 transition-transform
  duration-300 ease-in-out group-hover:translate-x-0.5'
       />
     </div>
